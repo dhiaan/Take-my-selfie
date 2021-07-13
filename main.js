78cfe238a3f93802ed8614bc9305945ccf79e6fe
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-07-13 17:51:58 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
-
+var Textbox = document.getElementById("textbox"); 
 function start()
 {
- document.getElementById("textbox").innerHTML = "";
- recognition.start();
+    Textbox.innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    Textbox.innerHTML = Content;
+    console.log(Content);
+        console.log("taking selfie --- ");
+        speak();
+      
 }
 
-recognition.onresult = function run (event) {
 
- console.log (event);
+function speak(){
+    var synth = window.speechSynthesis;
 
-var Content = event.result[0][0].transcipt
-console.log(Content);
+    speak_data = "Taking you Selfie in 5 seconds";
 
-document.getElementById("textbox").innerHTML = Content;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_selfie(); 
+        save();
+    }, 5000);
 }
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
 
